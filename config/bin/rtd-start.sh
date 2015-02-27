1358737aeb1328544556cefb5f2eda104a24dce7
Jean-François Roche <jfroche@affinitic.be>
2015-02-27 17:14:53 +0100

    start celeryd daemon

--- config/bin/rtd-start.sh
+++ config/bin/rtd-start.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
+
+curl -XPUT 'http://elasticsearch:9200/readthedocs/'
 
 cd /app/readthedocs
 
@@ -9,5 +11,5 @@ echo "from django.contrib.auth.models import User; User.objects.create_superuser
 $PYTHON manage.py loaddata test_data
 $PYTHON manage.py makemessages --all
 $PYTHON manage.py compilemessages
-
+/venv/bin/python manage.py celeryd -l INFO &
 /venv/bin/python manage.py runserver 0.0.0.0:8000
