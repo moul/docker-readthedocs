2c664e444dd7498e944be59f5d4cfa9a29de44c8
Jean-François Roche <jfroche@affinitic.be>
2015-02-27 17:15:58 +0100

    configure elasticsearch and redis access

--- config/etc/default/rtd-config.py
+++ config/etc/default/rtd-config.py
@@ -14,4 +14,14 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-SITE_ROOT = '/app/readthedocs'
+SITE_ROOT = '/app'
+ES_HOSTS = ['elasticsearch:9200']
+REDIS = {
+    'host': 'redis',
+    'port': 6379,
+    'db': 0,
+}
+BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+DEBUG = True
+CELERY_ALWAYS_EAGER = False
