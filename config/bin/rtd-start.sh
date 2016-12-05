8d89aabc734d814a560fd96035e74e920aa39e6f
Maxim Lanin <max@lanin.me>
2016-12-05 12:04:49 +0300

    Fix celeryd startup
    Celery will throw exception if you'll try to start it's daemon from the root. This env var will force it to start.

--- config/bin/rtd-start.sh
+++ config/bin/rtd-start.sh
@@ -12,5 +12,7 @@ echo "from django.contrib.auth.models import User; User.objects.create_superuser
 $PYTHON manage.py loaddata test_data
 $PYTHON manage.py makemessages --all
 $PYTHON manage.py compilemessages
+
+export C_FORCE_ROOT="true"
 /venv/bin/python manage.py celeryd -l INFO &
 /venv/bin/python manage.py runserver 0.0.0.0:8000
